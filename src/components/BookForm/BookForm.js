76d117214fe95ff0f3ef97bf3e2815c2bc9655de
ashraffares <ashraffares090@gmail.com>
2021-08-31 14:30:57 +0200

    Add title and category it handled by bookform state

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,20 +1,27 @@
-const BookForm = () => (
-  <form>
-    <input type="text" />
-    <label htmlFor="category">
-      Choose a category:
-      <select name="category" id="category">
-        <option value="Action">Action</option>
-        <option value="Biography">Biography</option>
-        <option value="History">History</option>
-        <option value="Horror">Horror</option>
-        <option value="Kids">Kids</option>
-        <option value="Learning">Learning</option>
-        <option value="Sci-Fi">Sci-Fi</option>
-      </select>
-    </label>
-    <button type="submit">Submit</button>
-  </form>
-);
+import { useState } from "react";
+
+const BookForm = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+
+  return (
+    <form>
+      <input type="text" />
+      <label htmlFor="category">
+        Choose a category:
+        <select name="category" id="category">
+          <option value="Action">Action</option>
+          <option value="Biography">Biography</option>
+          <option value="History">History</option>
+          <option value="Horror">Horror</option>
+          <option value="Kids">Kids</option>
+          <option value="Learning">Learning</option>
+          <option value="Sci-Fi">Sci-Fi</option>
+        </select>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 export default BookForm;
